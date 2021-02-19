e6c3ae65fe8761906e0014ed13c8ee4a29707217
Morgan Smith <Morgan.J.Smith@outlook.com>
2021-02-19 09:13:47 -0500

    add max86150 research

---
+++ src/max86150.c
@@ -0,0 +1,30 @@
+
+/* Raw PPG data to BPM conversion
+ *
+ *
+ * Python (LGPL-3.0-or-later)
+ * https://github.com/daniel-thompson/wasp-os/blob/master/wasp/ppg.py
+ *
+ *
+ * C++ (LGPL-3.0-or-later) (is a port of the python conversion code)
+ * https://github.com/JF002/Pinetime/blob/develop/src/components/heartrate/Ppg.cpp
+ *
+ */
+
+
+/* Existing drivers
+ *
+ *
+ * C++ (Expat License)
+ * https://github.com/Protocentral/protocentral_max86150_ecg_ppg/blob/master/src/max86150.cpp
+ *
+ *
+ * Arduino (Expat License)
+ *
+ * Seems to include the above driver by protocentral. Also I'm not sure what
+ * the difference between these two repositories are.
+ *
+ * https://github.com/Mic-Tsai/Sensor_ECG_MAX86150_Patch
+ * https://github.com/Mic-Tsai/Sensor_ECG_PPG_MAX86150_Dev-Board
+ *
+ */
